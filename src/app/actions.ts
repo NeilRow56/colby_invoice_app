581ad4cfba53fbc1c7cddd14d3a676f00c5a6930
NeilRow56 <neilrowland56@gmail.com>
2024-12-02 11:53:24 +0000

    Updated action to make a post to xata database

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,11 +5,16 @@ import { Invoices } from '@/db/schema'
 
 //Create invoice
 export async function createAction(formData: FormData) {
-  // const value = Math.floor(
-  //     Number.parseFloat(String(formData.get("value"))) * 100,
-  //   );
-  //   const description = formData.get("description") as string;
-  //   const name = formData.get("name") as string;
-  //   const email = formData.get("email") as string;
-  console.log('formData', formData)
+  const value = Math.floor(
+    Number.parseFloat(String(formData.get('value'))) * 100
+  )
+  const description = formData.get('description') as string
+  //   const name = formData.get('name') as string
+  //   const email = formData.get('email') as string
+
+  await db.insert(Invoices).values({
+    value,
+    description,
+    status: 'open'
+  })
 }
